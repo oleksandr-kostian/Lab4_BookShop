b069f70c6d2e71af5415bed2b43f97a3dfbcd2b2
Sasha Kostyan <kostyansasha@ukr.net>
2016-07-07 17:33:33 +0300

    delete unnecessary sout.

--- ejb/src/main/java/com/beans/order/OrderBean.java
+++ ejb/src/main/java/com/beans/order/OrderBean.java
@@ -104,44 +104,26 @@ public class OrderBean implements EntityBean {
     }
 
     public void ejbRemove() throws RemoveException, EJBException {
-
-
-        /*
-        Method are rewriting now
-         */
-
         System.out.println("OrderRemote bean method ejbRemove() was called.");
 
         Connection connection = DataSourceConnection.getInstance().getConnection();
         ResultSet result = null;
         PreparedStatement statement = null;
 
-        System.out.println("-1 idOrder=" + this.getIdOrder());
-
         try {
-            statement = connection.prepareStatement("DELETE ORDERS WHERE ID_ORDER = ?");  //????????????????????
+            statement = connection.prepareStatement("DELETE ORDERS WHERE ID_ORDER = ?");
             statement.setInt(1, this.getIdOrder());
-
-            System.out.println("-2");
 
             statement.execute();
             statement = null;
             result = null;
 
-            System.out.println("-2.1");
-            System.out.println("-3 size="+this.getContents().size());
-
             for(int i = 1; i < this.getContents().size(); i++){
-                System.out.println("-3.1");
-
                 statement = connection.prepareStatement("DELETE CONTENR_ORDER WHERE ID_ORDER = ?");
                 statement.setInt(1, this.getIdOrder());
                 statement.execute();
                 statement = null;
-
-                System.out.println("-3.1---");
             }
-            System.out.println("-4");
         } catch (SQLException e) {
             throw new EJBException("Can't delete data", e);
         } finally {
